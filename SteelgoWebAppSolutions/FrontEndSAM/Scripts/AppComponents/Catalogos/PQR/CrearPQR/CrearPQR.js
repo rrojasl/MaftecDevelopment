9719b68cf2aec76a4954d9c719853d34638a476b
JoseColmenero <jcolmenero@maftec.com.mx>
2016-05-19 13:59:13 -0500

    Vista para agregar PQRs

---
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/Catalogos/PQR/CrearPQR/CrearPQR.js
@@ -0,0 +1,53 @@
+﻿
+function CargaInicial() {
+    ConvertirCombos();
+    setTimeout(function () { ObtenerListasPQR() }, 500);
+};
+
+CargaInicial();
+
+function ConvertirCombos() {
+
+    $("#ProcesoSoldaduraRellenoID").kendoComboBox({
+        dataTextField: "Codigo",
+        dataValueField: "ProcesoSoldaduraID",
+        select: function (e) {
+            dataItem = this.dataItem(e.item.index());
+        },
+    });
+
+    $("#ProcesoSoldaduraRaizID").kendoComboBox({
+        dataTextField: "Codigo",
+        dataValueField: "ProcesoSoldaduraID",
+        select: function (e) {
+            dataItem = this.dataItem(e.item.index());
+        },
+    });
+
+    $("#GrupoPMaterialBase1ID").kendoComboBox({
+        dataTextField: "GrupoP",
+        dataValueField: "GrupoPID",
+        select: function (e) {
+            dataItem = this.dataItem(e.item.index());
+            var PQRIDBuscar = dataItem.PQRID;
+        },
+    });
+
+    $("#GrupoPMaterialBase2ID").kendoComboBox({
+        dataTextField: "GrupoP",
+        dataValueField: "GrupoPID",
+        select: function (e) {
+            dataItem = this.dataItem(e.item.index());
+            var PQRIDBuscar = dataItem.PQRID;
+        },
+    });
+
+    $("#CodigoID").kendoComboBox({
+        dataTextField: "Especificacion",
+        dataValueField: "CodigoAsmeID",
+        select: function (e) {
+            dataItem = this.dataItem(e.item.index());
+        },
+    });
+
+};
