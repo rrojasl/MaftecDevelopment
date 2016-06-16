17a49ed3cff1c6f6298bd741479007f47e0fa378
Eduardo Raul Martinez Dominguez <edu_09_11_91@hotmail.com>
2016-06-16 17:59:49 -0500

    se agregar PreventDefault en renders de SC

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/WPS/Listado/Render.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/WPS/Listado/Render.js
@@ -11,6 +11,7 @@
             dataSource: options.model.listadoRaizPQR,
             template: "<i class=\"fa fa-#=data.Nombre#\"></i> #=data.Nombre#",
             change: function (e) {
+                e.preventDefault();
                 dataItem = this.dataItem(e.sender.selectedIndex);
                 if (dataItem != undefined && dataItem.Nombre != "") {
                     options.model.NombrePQRRaiz = dataItem.Nombre;
@@ -19,7 +20,7 @@
                     if (Boolean(options.model.PWHTRellenoId) != dataItem.PWHT) {
                         cadenaError += "\n " + _dictionary.WPSMensajeErrorPWHT[$("#language").data("kendoDropDownList").value()];
                     }
-                    if (ContieneGruposMaterialBase(dataItem.GrupoPMaterialBase1,dataItem.GrupoPMaterialBase2, options.model.GrupoMaterialBase1RellenoUID, options.model.GrupoMaterialBase1RellenoDID)) {
+                    if (ContieneGruposMaterialBase(dataItem.GrupoPMaterialBase1, dataItem.GrupoPMaterialBase2, options.model.GrupoMaterialBase1RellenoUID, options.model.GrupoMaterialBase1RellenoDID)) {
                         cadenaError += "\n " + _dictionary.WPSMensajeErrorGrupoP[$("#language").data("kendoDropDownList").value()];
                     }
                     if (options.model.PREHEATRellenoId != dataItem.PREHEAT) {
@@ -28,16 +29,16 @@
                     if (cadenaError != "") {
                         displayNotify("", cadenaError, "1");
                     }
-                        
 
-                    options.model.PWHTRaiz = dataItem.PWHT ;
+
+                    options.model.PWHTRaiz = dataItem.PWHT;
                     options.model.PWHTRaizId = dataItem.PWHT == true ? 1 : 0;
                     options.model.GrupoMaterialBase1RaizUID = dataItem.GrupoPMaterialBase1;
                     options.model.GrupoMaterialBase1RaizU = dataItem.GrupoPMaterialBase1Nombre;
                     options.model.GrupoMaterialBase1RaizDID = dataItem.GrupoPMaterialBase2;
                     options.model.GrupoMaterialBase1RaizD = dataItem.GrupoPMaterialBase2Nombre;
                     options.model.GrupoPRaiz = dataItem.GrupoPMaterialBase1Nombre + " " + dataItem.GrupoPMaterialBase2Nombre;
-                    options.model.PREHEATRaiz = dataItem.PREHEAT == 1? true: false;
+                    options.model.PREHEATRaiz = dataItem.PREHEAT == 1 ? true : false;
                     options.model.PREHEATRaizId = dataItem.PREHEAT;
                     var rootMax = ObtenerEspesorCorrecto((parseFloat(dataItem.EspesorRaiz) + parseFloat(dataItem.EspesorRelleno)), dataItem.PREHEAT, dataItem.CodigoRaiz.trim(), true);
                     var fillMax = ObtenerEspesorCorrecto((parseFloat(options.model.RellenoEspesorRaiz) + parseFloat(options.model.RellenoEspesorRelleno)), dataItem.PREHEAT, dataItem.CodigoRaiz.trim(), false);
@@ -50,16 +51,21 @@
                 }
                 else {
                     options.model.NombrePQRRaiz = "";
+                    options.model.PQRRaizId = 0;
                     options.model.PWHTRaiz = false;
+                    options.model.PWHTRaizId = 0;
+                    options.model.GrupoMaterialBase1RaizUID = 0;
                     options.model.GrupoMaterialBase1RaizU = "";
+                    options.model.GrupoMaterialBase1RaizDID = 0;
                     options.model.GrupoMaterialBase1RaizD = "";
                     options.model.PREHEATRaiz = false;
+                    options.model.PREHEATRaizId = 0;
                     options.model.EspesorMaximo = 0;
                     options.model.EspesorMinimo = 0;
                     options.model.RaizEspesorRaiz = 0;
                     options.model.RaizEspesorRelleno = 0;
                     try {
-                        $("#grid").data("kendoGrid").dataSource.sync();
+                        //$("#grid").data("kendoGrid").dataSource.sync();
                     }
                     catch (e) {
                     }
@@ -98,6 +104,7 @@ function RenderComboBoxPQRRelleno(container, options) {
             dataSource: options.model.listadoRellenoPQR,
             template: "<i class=\"fa fa-#=data.Nombre#\"></i> #=data.Nombre#",
             change: function (e) {
+                e.preventDefault();
                 dataItem = this.dataItem(e.sender.selectedIndex);
                 if (dataItem != undefined && dataItem.Nombre != "") {
                     options.model.NombrePQRRelleno = dataItem.Nombre;
@@ -106,7 +113,7 @@ function RenderComboBoxPQRRelleno(container, options) {
                     if (Boolean(options.model.PWHTRaizId) != dataItem.PWHT) {
                         cadenaError += "\n " + _dictionary.WPSMensajeErrorPWHT[$("#language").data("kendoDropDownList").value()];
                     }
-                    if (ContieneGruposMaterialBase(dataItem.GrupoPMaterialBase1,dataItem.GrupoPMaterialBase2, options.model.GrupoMaterialBase1RaizUID, options.model.GrupoMaterialBase1RaizDID)) {
+                    if (ContieneGruposMaterialBase(dataItem.GrupoPMaterialBase1, dataItem.GrupoPMaterialBase2, options.model.GrupoMaterialBase1RaizUID, options.model.GrupoMaterialBase1RaizDID)) {
                         cadenaError += "\n " + _dictionary.WPSMensajeErrorGrupoP[$("#language").data("kendoDropDownList").value()];
                     }
                     if (options.model.PREHEATRaizId != dataItem.PREHEAT) {
@@ -122,7 +129,7 @@ function RenderComboBoxPQRRelleno(container, options) {
                     options.model.GrupoMaterialBase1RellenoDID = dataItem.GrupoPMaterialBase2;
                     options.model.GrupoMaterialBase1RellenoD = dataItem.GrupoPMaterialBase2Nombre;
                     options.model.GrupoPRelleno = dataItem.GrupoPMaterialBase1Nombre + " " + dataItem.GrupoPMaterialBase2Nombre;
-                    options.model.PREHEATRelleno = dataItem.PREHEAT == 1 ? true: false;
+                    options.model.PREHEATRelleno = dataItem.PREHEAT == 1 ? true : false;
                     options.model.P1REHEATRellenoId = dataItem.PREHEAT;
                     var rootMax = ObtenerEspesorCorrecto((parseFloat(dataItem.EspesorRaiz) + parseFloat(dataItem.EspesorRelleno)), dataItem.PREHEAT, dataItem.CodigoRaiz.trim(), true);
                     var fillMax = ObtenerEspesorCorrecto((parseFloat(options.model.RaizEspesorRaiz) + parseFloat(options.model.RaizEspesorRelleno)), dataItem.PREHEAT, dataItem.CodigoRaiz.trim(), false);
@@ -136,27 +143,32 @@ function RenderComboBoxPQRRelleno(container, options) {
                 }
                 else {
                     options.model.NombrePQRRelleno = "";
-                    options.model.GrupoMaterialBase1RellenoU = "";
-                    options.model.GrupoMaterialBase1RellenoD = "";
+                    options.model.PQRRellenoId = 0;
                     options.model.PWHTRelleno = false;
+                    options.model.PWHTRellenoId = 0;
+                    options.model.GrupoMaterialBase1RellenoUID = 0;
+                    options.model.GrupoMaterialBase1RellenoU = "";
+                    options.model.GrupoMaterialBase1RellenoDID = 0;
+                    options.model.GrupoMaterialBase1RellenoD = "";
                     options.model.PREHEATRelleno = false;
+                    options.model.PREHEATRellenoId = 0;
                     options.model.EspesorMaximo = 0;
                     options.model.EspesorMinimo = 0;
                     options.model.RellenoEspesorRaiz = 0;
                     options.model.RellenoEspesorRelleno = 0;
-                    try{
-                        //$("#grid").data("kendoGrid").dataSource.sync();   
+                    try {
+                        //$("#grid").data("kendoGrid").dataSource.sync();
                     }
                     catch (e) {
                     }
-                    
+
                 }
-             
+
             }
-            
+
         }
         );
-    
+
     loadingStop();
 
     $(".k-combobox").parent().on('mouseleave', function (send) {
@@ -179,6 +191,7 @@ function RendercomboBoxProyecto(container, options) {
             dataSource: options.model.ListaProyectos,
             template: "<i class=\"fa fa-#=data.Nombre.toLowerCase()#\"></i> #=data.Nombre#",
             select: function (e) {
+                e.preventDefault();
                 dataItem = this.dataItem(e.item.index());
                 if (dataItem != undefined) {
                     options.model.Proyecto = dataItem.Nombre;
@@ -189,6 +202,7 @@ function RendercomboBoxProyecto(container, options) {
                 }
             },
             change: function (e) {
+                e.preventDefault();
                 dataItem = this.dataItem(e.sender.selectedIndex);
                 if (dataItem != undefined) {
                     options.model.Proyecto = dataItem.Nombre;
