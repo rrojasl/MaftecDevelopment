df01ce027127677b3063e994adb40dfb5ecc2ae9
Eduardo Raul Martinez Dominguez <edu_09_11_91@hotmail.com>
2016-06-20 15:46:15 -0500

    Se crea Controller con sus vistas para PND

--- SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/WPS/Listado/WPS.js
+++ SteelgoWebAppSolutions/FrontEndSAM/Scripts/AppComponents/WPS/Listado/WPS.js
@@ -1,33 +1,4 @@
 ﻿
-Cookies.set("home", true, { path: '/' });
-Cookies.set("navegacion", "10015", { path: '/' });
-
-
-var $WPSSaveModel = {
-    listContainer: {
-        create: "",
-        list: "",
-        detail: "",
-        destroy: ""
-    },
-    properties: {
-        NombreWPS: {
-            visible: "#DivNombreWPS",
-            editable: "#NomnreWPS",
-            required: "#NomnreWPS"
-        },
-        PQRRaiz: {
-            visible: "#DivNombrePQRRaiz",
-            editable: "#PQRRaizNombre",
-            required: "#PQRRaizNombre"
-        },
-        PQRRelleno: {
-            visible: "#DivNombrePQRRelleno",
-            editable: "#PQRRellenoNombre",
-            required: "#PQRRellenoNombre"
-        }
-    }
-};
 
 var ItemSeleccionado;
 var resultadoJson;
